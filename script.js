2f07dc689be031e6525b96a22350dd8fa3b7a5d8
SophieHornik <144719454+SophieHornik@users.noreply.github.com>
2024-04-28 19:31:21 +0000

    base/fixed buttons in html/Js

---
+++ script.js
@@ -0,0 +1,36 @@
+let paragraph1 = document.getElementById('paragraph');
+let year = document.getElementById('year');
+let main = document.getElementById('main');
+let pic1 = document.getElementById('pic1');
+let pic2 = document.getElementById('pic2');
+let pic3 = document.getElementById('pic3');
+function show2007() {
+  paragraph.textContent = "On January 10th, 2007, Sophia Frederique Hornik was born at 9:42 pm in New Brunswik hospital. Her first name (Sophia) was after her Grandpa (Saul Hornik). Her middle name (Frederique) was after her papa (Fred Haberman). Sophia’s Older brother, Ethan, decided he did not like the name Sophia so he would only call her “Sophie”, which Sophie has gone by ever since. Sophie is now 17 years old and a student at Marlboro high school."
+  year.textContent = "2007"
+  main.src = "balloons.jpg"
+  pic1.src = "birth.jpg"
+  pic2.src = "preschoolbday.jpg"
+  pic3.src = "collagebday.jpg"
+}
+
+
+let paragraph2 = document.getElementById('paragraph');
+
+function show1776() {
+  paragraph.textContent = "On January 10th, 1776, Thomas Paine's seminal work, Common Sense, was published, sparking a revolutionary wave that would alter the course of American history. Paine's persuasive and straightforward writing style made complex political ideas accessible to the common people, advocating for independence from British rule. His powerful arguments and call for a republic resonated deeply with the American colonists, inspiring widespread support for the revolutionary cause. Common Sense served as a catalyst for the shift in public sentiment, galvanizing the movement for independence and laying the groundwork for the Declaration of Independence later that year. Paine's work not only articulated the grievances against British tyranny but also stirred a sense of unity and purpose among the colonists, ultimately contributing significantly to the birth of the United States of America."
+  year.textContent = "1776"
+  main.src = "thomasp.jpg"
+  pic1.src = "cs.jpg"
+  pic2.src = "csoutline.jpg"
+  pic3.src = "cscrown.jpg"
+
+}
+function show1861() {
+  paragraph.textContent = "On January 10, 1861, Florida officially seceded from the Union, becoming the third state to do so during the tumultuous period leading up to the American Civil War. The state's secession was driven by a combination of economic, social, and political factors, including concerns over the preservation of slavery and states' rights. Florida's decision to join the Confederate States of America intensified the regional tensions and marked a pivotal moment in the escalation toward the Civil War. The impact of Florida's secession was felt not only in the state itself but also across the nation as it fueled the growing divide between the Northern and Southern states. The secession of Florida, along with other Southern states, ultimately culminated in the deadliest conflict in American history, the Civil War, which lasted from 1861 to 1865 and had profound and lasting consequences for the nation."
+  year.textContent = "1861"
+  main.src = "flcw.jpg"
+  pic1.src = "flleaves.jpg"
+  pic2.src = "flmap.jpg"
+  pic3.src = "flflag.png"
+}
+function show1912() {
