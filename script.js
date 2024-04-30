58badeb4fd71798db881a455d4dd6dfcb24314cc
SophieHornik <144719454+SophieHornik@users.noreply.github.com>
2024-04-30 05:36:37 +0000

    text content education pre buttons

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let pic1 = document.getElementById('pic1');
 let pic2 = document.getElementById('pic2');
 let main2 = document.getElementById('main2');
 function showOverview() {
-  paragraph1.innerHTML= "<p>According to <a href='https://www.bls.gov/ooh/healthcare/veterinarians.htm#tab-2' class='links'>The U.S. Bureau Of Labor Statistics</a>, The main responsibilities of a vet is to: <ul><li> Examine animals to assess their health and diagnose problems</li><li>Treat and dress wounds</li><li>Perform surgery on animals</li> <li>Test for and vaccinate against diseases</li><li>Operate medical equipment, such as x-ray machines</li><li>Advise animal owners about general care, medical conditions, and treatments</li> <li>Prescribe medication</li> <li>Euthanize animals</li></ul><br> According to <a class='links' href='https://www.sgu.edu/blog/veterinary/where-do-veterinarians-work/'>St. George's University</a> Vet's can work in numerous locations including: <ul><li>Zoos and aquariums</li><li> Food-processing plants</li><li>Farms and ranches<li>Colleges and universities</li><li>Laboratories</li><li>Animal shelters</li><li>Racetracks</li><li>National parks and other open areas</li><li>Offices</li></ul></p>"
+  paragraph1.innerHTML= "<p>According to <a href='https://www.bls.gov/ooh/healthcare/veterinarians.htm#tab-2' class='links'>The U.S. Bureau Of Labor Statistics</a>, <br> <b>The main responsibilities of a vet is to:</b> <ul><li> Examine animals to assess their health and diagnose problems</li><li>Treat and dress wounds</li><li>Perform surgery on animals</li> <li>Test for and vaccinate against diseases</li><li>Operate medical equipment, such as x-ray machines</li><li>Advise animal owners about general care, medical conditions, and treatments</li> <li>Prescribe medication</li> <li>Euthanize animals</li></ul><br> According to <a class='links' href='https://www.sgu.edu/blog/veterinary/where-do-veterinarians-work/'>St. George's University</a><br> <b>Vet's can work in numerous locations including:</b><ul><li>Zoos and aquariums</li><li> Food-processing plants</li><li>Farms and ranches<li>Colleges and universities</li><li>Laboratories</li><li>Animal shelters</li><li>Racetracks</li><li>National parks and other open areas</li><li>Offices</li></ul></p>"
   year.textContent = "Overview"
   main.src = "vetwdog.jpg"
   pic1.src = "cutecat.jpg"
@@ -17,12 +17,12 @@ function showOverview() {
 
 
 function showEducation() {
-  paragraph1.innerHTML = "On January 10th, 1776, Thomas Paine's seminal work, Common Sense, was published, sparking a revolutionary wave that would alter the course of American history. Paine's persuasive and straightforward writing style made complex political ideas accessible to the common people, advocating for independence from British rule. His powerful arguments and call for a republic resonated deeply with the American colonists, inspiring widespread support for the revolutionary cause. Common Sense served as a catalyst for the shift in public sentiment, galvanizing the movement for independence and laying the groundwork for the Declaration of Independence later that year. Paine's work not only articulated the grievances against British tyranny but also stirred a sense of unity and purpose among the colonists, ultimately contributing significantly to the birth of the United States of America."
+  paragraph1.innerHTML = "<p>According to <a href='https://learn.org/articles/What_are_the_Education_Requirements_to_Be_a_Veterinarian.html' class='links'>Learn.Org</a>, in order to become a Veterinarian you must attend 4 years of vet school. Having a bachelors degree can increase your chances of getting into a vet school. Some schools have prerequisite courses you must have taken prior to attending vet school. <br> <b>Some common prerequisites are:</b> <ul><li>animal biology</li> <li>microbiology</li> <li>animal nutrition</li> <li>zoology</li> <li>systemic physiology</li></ul><br><p> <a href='www.aavmc.org' class='links'>The Association of American Veterinary Medical Colleges </a> provides a useful table that allows students to check the requirements for individual colleges<p> <br> <p><b>Some of the top vet schools in the United states are:</b></p>"
   title.textContent = "Education"
   main.src = "vetgrad.jpg"
   pic1.src = "studyingdr.jpg"
   pic2.src = "catanddog.jpg"
-  main2.src="catanddog.jpg"
+  main2.src="vethorse.jpg"
 
 }
 function showEarnings() {
@@ -31,5 +31,6 @@ function showEarnings() {
   main.src = "dogmoney.jpg"
   pic1.src = "dogkisscat.jpg"
   pic2.src = "catmoney.jpg"
+  main2.src="reversevet.jpg"
 }
 
